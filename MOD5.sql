9d292169c032dc2cc8c2c6acf25d1267abe391e9
Sylwia Dudek <daisy.gith@gmail.com>
2026-05-10 15:54:11 +0200

    mod5
    istructions

---
+++ MOD5.sql
@@ -0,0 +1,22 @@
+/*Zad1.
+Napisz program, który za pomocą wyrażenia CASE wygeneruje wyjątek CASE_NOT_FOUND. 
+Obsłuż go wyświetlając na konsoli informację o kodzie błędu ORA i komunikacie błędu ORA.
+ W nowej linii konsoli napisz użytkownikowi jaką akcję powinien podjąć by pozbyć się tego błędu.
+
+Zad2.
+Napisz program PL/SQL, który będzie dodawał do tabeli countries nowy rekord o następujących wartościach:
+country_id: ML
+country_name: Malta
+Obsłuż komunikat błędu o naruszeniu constraintu wykorzystując klauzulę PRAGMA EXCEPTION_INIT.
+
+Zad3.
+Wykorzystaj program z poprzedniej pracy domowej liczący wskaźnik BMI. Zadeklaruj w nim własny wyjątek o nazwie ex_too_big. 
+Jeśli wzrost użytkownika jest wyższy niż 225cm rzuć zdefiniowany wyjątek (ex_too_big).
+Program powinien w takiej sytuacji zwracać przyjazną użytkownikowi informację, że podany wzrost jest nienaturalnie duży i należy wprowadzić poprawną wartość.
+
+Zad4.
+Podaj link do dokumentacji z listą systemowych predefiniowanych wyjątków w bazie danych Oracle 18c.
+
+Zad5.
+Jakie 3 rodzaje wyjątków istnieją w bazie danych Oracle i czym się od siebie różnią?
+*/
