88ad586a7f7871d0d81c948fdf31ceac3e5b4ba8
Sylwia Dudek <daisy.gith@gmail.com>
2026-09-09 20:15:26 +0200

    mod9

---
+++ MOD9.sql
@@ -0,0 +1,16 @@
+/*
+1. Zamień blok anonimowy z pracy domowej z MODUŁ 5 ZADANIE 3 na funkcję o nazwie licz_bmi. Funkcja powinna zwracać wskaźnik BMI i przyjmować w parametrze wzrost i wagę.
+Uwaga. W funkcji powinna się znaleźć jedynie jedna instrukcja RETURN.
+2. Wyjaśnij czym różni się RETURN, RETURN wartość,  EXIT.
+3. Napisz program, który sprawdzi czy podana w parametrze liczba jest parzysta czy nie. Wywołaj funkcję na 2 sposoby:
+w poleceniu SELECT dla wszystkich id pracownika z tabeli employees
+w programie PL/SQL w pętli zwracającej liczby od 1 do 10
+4. Napisz funkcję, która zwróci nową pensję pracownika w zależności od jego aktualnych zarobków, tj. jeśli pracownik:
+zarabia mniej niż 5000 to podwyżka o 20%
+zarabia między 5000 a 7000 to podwyżka o 15%
+pozostali podwyżka o 10%
+Funkcję zwracającą nową pensję wykorzystaj w procedurze aktualizuj_pensję, która będzie modyfikowała aktualną wysokość pensji pracownika na tą zwracaną przez funkcję. 
+Wywołaj procedurę aktualizuj_pensję dla departamentu 50.
+
+***Dla chętnych. Zamień powyższy program na pojedynczą instrukcję UPDATE.
+*/
